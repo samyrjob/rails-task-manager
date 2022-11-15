62f445df3dbcaae4e3bc808e5b187ba75530d437
Samy Rjob <s.rjob@audencia.com>
2022-11-15 15:52:35 +0100

    ee

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+#   #create task
+#   get "tasks/new", to: "tasks#new", as: "new_task"
+#   post "/tasks" , to: "tasks#create"
+
+
+# #update task
+
+#   get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+#   patch "tasks/:id", to: "tasks#update"
+
+
+#   get "/tasks", to: "tasks#index"
+
+#   get "tasks/:id", to: "tasks#show", as: "task"
+
+# #delete
+
+# delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
+
 end
